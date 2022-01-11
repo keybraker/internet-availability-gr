14b707a173901979ff09224f604765ff9d3d0a6b
Keybraker <iantsiakkas@gmail.com>
2022-01-11 15:19:58 +0200

    implemented a crawler to get street, area names before final release

--- crawler.js
+++ crawler.js
@@ -1,15 +1,38 @@
 const Crawler = require('crawler');
+const editJsonFile = require("edit-json-file");
+const data = require('./database.json');
 
-const addresses = [{
-    address: 'ΘΕΡΜΟΠΥΛΩΝ (ΟΔΌΣ)',
-    state: 'Ν. ΑΧΑΪΑΣ',
-    prefecture: 'Δ. ΠΑΤΡΕΩΝ',
-    number: 12,
-    area: 'ΠΑΤΡΑ-ΚΩΝΣΤΑΝΤΙΝΟΥΠΟΛΕΩΣ'
-}]
+let file = editJsonFile(`${__dirname}/database.json`);
+let database = file.toObject();
+
+// const prefectures = {
+//     name: "Δ. ΗΡΑΚΛΕΙΟΥ",
+//     list: [
+//         "Δ.ΗΡΑΚΛΕΙΟΥ"
+//     ]
+// }
+
+// const addresses = [{
+//     address: 'ΠΑΤΡΙΑΡΧΟΥ ΓΡΗΓΟΡΙΟΥ Ε\' (ΟΔΌΣ)',
+//     state: 'Ν. ΗΡΑΚΛΕΙΟΥ',
+//     prefecture: 'Δ. ΗΡΑΚΛΕΙΟΥ',
+//     number: 16,
+//     area: 'ΜΑΣΤΑΜΠΑΣ'
+// }];
+
+const queueList = data.streets
+    .filter(street => street.area === null)
+    .map(street => {
+        return `https://www.cosmote.gr/eshop/global/gadgets/populateAddressDetailsV3.jsp?` +
+            `streetName=${encodeURI(street.name)}&` +
+            `stateId=48&` +
+            `&municipalityId=893&` +
+            `_=1641891804001`
+    });
 
 const c = new Crawler({
     maxConnections: 10,
+    rateLimit: 1000,
 
     // This will be called for each crawled page
     callback: (error, res, done) => {
@@ -20,27 +43,41 @@ const c = new Crawler({
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server
 
-            if ($.text().replace(/\s/g, '').includes('Έως200Mbps')) { console.log('Έως 200 Mbps'); }
-            if ($.text().replace(/\s/g, '').includes('Έως100Mbps')) { console.log('Έως 100 Mbps'); }
-            if ($.text().replace(/\s/g, '').includes('Έως50Mbps')) { console.log('Έως 50 Mbps'); }
-            if ($.text().replace(/\s/g, '').includes('Έως24Mbps')) { console.log('Έως 24 Mbps'); }
+            let uri = res.options.uri;
+            uri = uri.substring(84);
+            uri = uri.substring(0, uri.indexOf("&stateId"));
+
+            const street = decodeURI(uri);
+
+            const entry = database.streets.find(s => s.name === street);
+            if (entry) {
+                console.log(`area for ${street} is `, $.text().replace(/\s/g, '').substring(7));
+
+                entry.area = $.text().replace(/\s/g, '').substring(7);
+                file.write(JSON.stringify(database));
+            }
+
+            if ($.text().replace(/\s/g, '').includes('Έως200MbpsΔιαθέσιμοστηνπεριοχήσου!')) {
+                console.log('Έως 200 Mbps');
+            }
+            if ($.text().replace(/\s/g, '').includes('Έως100MbpsΔιαθέσιμοστηνπεριοχήσου!')) {
+                console.log('Έως 100 Mbps');
+            }
+            if ($.text().replace(/\s/g, '').includes('Έως50MbpsΔιαθέσιμοστηνπεριοχήσου!')) {
+                console.log('Έως  50 Mbps');
+            }
+            if ($.text().replace(/\s/g, '').includes('Έως24MbpsΔιαθέσιμοστηνπεριοχήσου!')) {
+                console.log('Έως  24 Mbps');
+            }
         }
-        done();
+
+        done(() => {
+            file.save();
+            file = editJsonFile(`${__dirname}/database.json`, {
+                autosave: true
+            });
+        });
     }
 });
 
-// Queue just one URL, with default callback
-addresses.forEach(address => c.queue(`https://www.cosmote.gr/selfcare/jsp/ajax/avdslavailabilityAjaxV2.jsp?` +
-    `Accept-Language=en-US,en;q=0.9,el-GR;q=0.8,el;q=0.7` +
-    `mTelno=&m` +
-    `Address=${encodeURI(address.address)}&m` +
-    `State=${encodeURI(address.state)}&m` +
-    `Prefecture=${encodeURI(address.prefecture)}&m` +
-    `Number=22&m` +
-    `Area=${encodeURI(address.area)}&` +
-    `searchcriteria=address&` +
-    `ct=bus`
-));
-
-//${encodeURI('Δ. ΠΑΤΡΕΩΝ')}&m
-//Έως 200 Mbps
+c.queue(queueList);
